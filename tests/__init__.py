196680900c1895f0fe7167032d62eb1a82abc244
milad <miladvf2014@gmail.com>
2026-05-07 21:15:57 +0330

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+Test suite for Story Development Toolkit.
+
+Author: Milad Rezanezhad
+GitHub: https://github.com/miladrezanezhad
+"""
+
+import sys
+from pathlib import Path
+
+# Add parent directory to path
+sys.path.insert(0, str(Path(__file__).parent.parent))
