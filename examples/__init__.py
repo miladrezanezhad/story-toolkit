f5f38ddb2607a6cd2278392f98447a4e7dcaf1ac
milad <miladvf2014@gmail.com>
2026-05-07 21:09:29 +0330

    Create __init__.py

---
+++ examples/__init__.py
@@ -0,0 +1,13 @@
+"""
+Usage examples for Story Development Toolkit.
+Contains basic, advanced, and quick-start examples.
+
+Author: Milad Rezanezhad
+GitHub: https://github.com/miladrezanezhad
+"""
+
+from .simple_example import quick_start
+from .example import main as run_full_example
+from .advanced_example import main as run_advanced_example
+
+__all__ = ['quick_start', 'run_full_example', 'run_advanced_example']
